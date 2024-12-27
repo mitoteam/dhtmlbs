6397cc7eccdcd3699d13490a2ad3b1f7caa8d3a3
Mikhail Oleynik <mishutka@mito-team.com>
2024-12-27 14:32:22 +0300

    input types

--- form_controls.go
+++ form_controls.go
@@ -7,18 +7,44 @@ import (
 )
 
 const (
+	inputControlKind         = "bs_input"
 	floatingInputControlKind = "bs_floatinginput"
 	submitControlKind        = "bs_submit_btn"
 )
 
+var classBlockError dhtml.Classes = dhtml.NewClasses("border border-danger bg-danger")
+
 func init() {
+	dhtmlform.RegisterFormControlHandler(inputControlKind, &dhtmlform.FormControlHandler{
+		RenderF: func(control *dhtmlform.FormControlElement) (out dhtml.HtmlPiece) {
+			rootTag := formControlWrapper(control)
+
+			if !control.GetLabel().IsEmpty() {
+				rootTag.Append(renderControlLabel(control))
+			}
+
+			inputTag := dhtml.NewTag("input").Id(control.GetId()).Class("form-control").
+				Attribute("type", control.GetProp("type").(string)).
+				Attribute("name", control.Name).Attribute("value", mttools.AnyToString(control.GetValue()))
+
+			if control.GetPlaceholder() != "" {
+				inputTag.Attribute("placeholder", control.GetPlaceholder())
+			}
+
+			rootTag.Append(inputTag)
+
+			if !control.GetNote().IsEmpty() {
+				rootTag.Append(renderControlNote(control))
+			}
+
+			out.Append(rootTag)
+			return out
+		},
+	})
+
 	dhtmlform.RegisterFormControlHandler(floatingInputControlKind, &dhtmlform.FormControlHandler{
 		RenderF: func(control *dhtmlform.FormControlElement) (out dhtml.HtmlPiece) {
-			rootTag := dhtml.Div().Class("form-floating mb-3")
-
-			if control.IsError() {
-				rootTag.Class(classesBlockError)
-			}
+			rootTag := formControlWrapper(control).Class("form-floating")
 
 			inputTag := dhtml.NewTag("input").Id(control.GetId()).Class("form-control").
 				Attribute("type", control.GetProp("type").(string)).
@@ -50,7 +76,7 @@ func init() {
 
 	dhtmlform.RegisterFormControlHandler(submitControlKind, &dhtmlform.FormControlHandler{
 		RenderF: func(control *dhtmlform.FormControlElement) (out dhtml.HtmlPiece) {
-			tag := dhtml.NewTag("button").Attribute("type", "submit").Class("btn btn-primary")
+			tag := dhtml.NewTag("button").Attribute("type", "submit").Class("btn btn-success")
 
 			if !mttools.IsEmpty(control.GetValue()) {
 				tag.Attribute("name", control.Name).Attribute("value", mttools.AnyToString(control.GetValue()))
@@ -68,6 +94,38 @@ func init() {
 	})
 }
 
+func NewTextInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "text")
+}
+
+func NewPasswordInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "password")
+}
+
+func NewEmailInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "email")
+}
+
+func NewDateInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "date")
+}
+
+func NewNumberInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "number")
+}
+
+func NewTelInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "tel")
+}
+
+func NewTimeInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "time")
+}
+
+func NewUrlInput(name string) *dhtmlform.FormControlElement {
+	return dhtmlform.NewFormControl(inputControlKind, name).SetProp("type", "url")
+}
+
 func NewFloatingTextInput(name string) *dhtmlform.FormControlElement {
 	return dhtmlform.NewFormControl(floatingInputControlKind, name).SetProp("type", "text")
 }
@@ -78,4 +136,46 @@ func NewFloatingPasswordInput(name string) *dhtmlform.FormControlElement {
 
 func NewSubmitBtn() *dhtmlform.FormControlElement {
 	return dhtmlform.NewFormControl(submitControlKind, "submit")
+}
+
+// -------------------- internal helpers --------------------------
+
+func formControlWrapper(controlElement *dhtmlform.FormControlElement) (tag *dhtml.Tag) {
+	tag = dhtml.Div()
+
+	var wrapperClass any
+
+	if controlElement.HasProp("wrapper-class") {
+		wrapperClass = controlElement.GetProp("wrapper-class")
+	} else {
+		wrapperClass = "mb-3"
+	}
+
+	if !mttools.IsEmpty(wrapperClass) {
+		tag.Class(wrapperClass)
+	}
+
+	if controlElement.IsError() {
+		tag.Class(classBlockError)
+	}
+
+	return tag
+}
+
+func renderControlLabel(controlElement *dhtmlform.FormControlElement) (out dhtml.HtmlPiece) {
+	labelElement := dhtml.NewLabel().For(controlElement.GetId()).Class("form-label").
+		Append(controlElement.GetLabel())
+
+	if controlElement.IsRequired() {
+		labelElement.Append(dhtml.Span().Class("text-danger fw-bold ms-1").Text("*"))
+	}
+
+	out.Append(labelElement)
+
+	return out
+}
+
+func renderControlNote(controlElement *dhtmlform.FormControlElement) (out dhtml.HtmlPiece) {
+	out.Append(dhtml.Div().Class("small text-muted").Append(controlElement.GetNote()))
+	return out
 }
